a66afe65c8abef5a15d2ecc6d755dc0aba274dfb
GorskiJakub <jakubg93@o2.pl>
2016-02-14 20:04:16 +0100

    dodawanie nowych produktow za posrednictwem aplikacji

---
+++ app/src/main/java/com/example/dell/listazakupow/NewProductActivity.java
@@ -0,0 +1,143 @@
+package com.example.dell.listazakupow;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class NewProductActivity extends Activity {
+
+    // Progress Dialog
+    private ProgressDialog pDialog;
+
+    JSONParser jsonParser = new JSONParser();
+    EditText inputName;
+    EditText inputPrice;
+    EditText inputDesc;
+
+    Integer current_list_id = getIntent().getIntExtra("list_id",0);
+
+
+    // url to create new product
+    private static String url_create_product = "https://eu-cdbr-azure-north-d.cloudapp.net:3306/lista_zakupow/create_product.php";
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_product);
+
+        // Edit Text
+        inputName = (EditText) findViewById(R.id.inputName);
+        inputPrice = (EditText) findViewById(R.id.inputPrice);
+        inputDesc = (EditText) findViewById(R.id.inputDesc);
+
+        // Create button
+        Button btnCreateProduct = (Button) findViewById(R.id.btnCreateProduct);
+
+        // button click event
+        btnCreateProduct.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // creating new product in background thread
+                String name = inputName.getText().toString();
+                String price = inputPrice.getText().toString();
+                String description = inputDesc.getText().toString();
+                String id_list = current_list_id.toString();
+                new CreateNewProduct().execute(name, price, description, id_list);
+            }
+        });
+    }
+
+    /**
+     * Background Async Task to Create new product
+     * */
+    class CreateNewProduct extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(NewProductActivity.this);
+            pDialog.setMessage("Creating Product..");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        /**
+         * Creating product
+         * */
+        protected String doInBackground(String... args) {
+            String name = args[0];
+            String price = args[1];
+            String description = args[2];
+            String id_list = args[3];
+
+
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("name", name));
+            params.add(new BasicNameValuePair("id_list", id_list));
+            params.add(new BasicNameValuePair("price", price));
+            params.add(new BasicNameValuePair("description", description));
+
+            // getting JSON Object
+            // Note that create product url accepts POST method
+            JSONObject json = jsonParser.makeHttpRequest(url_create_product,
+                    "POST", params);
+
+            // check log cat from response
+            Log.d("Create Response", json.toString());
+
+            // check for success tag
+            try {
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // successfully created product
+                    Intent i = new Intent(getApplicationContext(), AllProductsActivity.class);
+                    startActivity(i);
+
+                    // closing this screen
+                    finish();
+                } else {
+                    // failed to create product
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once done
+            pDialog.dismiss();
+        }
+
+    }
+}
+
