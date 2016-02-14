7e0545457297b96e8919732db6a6e85ec2a90c25
GorskiJakub <jakubg93@o2.pl>
2016-02-14 19:36:33 +0100

    pliki zapewniajace polaczenie z baza

---
+++ lista_zakupow/db_config.php
@@ -0,0 +1,11 @@
+<?php
+ 
+/*
+ * All database connection variables
+ */
+ 
+define('DB_USER', "bea61ca66d2766"); // db user
+define('DB_PASSWORD', "524686ce"); // db password
+define('DB_DATABASE', "lista_zakupow"); // database name
+define('DB_SERVER', "eu-cdbr-azure-north-d.cloudapp.net:3306"); // db server
+?>
